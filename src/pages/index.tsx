c16acd42fc74756643ebf396a21e400956326fa1
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-03-09 01:43:36 -0300

    feature: SSG api stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
@@ -35,7 +35,7 @@ export default function Home({product}: HomeProps) {
 }
 
 
-export const getServerSideProps:GetServerSideProps = async () => {
+export const getStaticProps:GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KbFvRFRXZ1K4ikNa6joF1Dx')
 
   const product = {
@@ -49,6 +49,7 @@ export const getServerSideProps:GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
