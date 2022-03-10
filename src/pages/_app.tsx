c9486643af70ffea8c868848ce45f92d30c4cc63
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-03-09 23:05:32 -0300

    feature: Next-auth authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from "next/app"
 import { Header } from "../components/Header"
+import { SessionProvider as NextAuthProvider} from 'next-auth/react'
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <>
-  <Header />
-  <Component {...pageProps} />
-  </>
+  <NextAuthProvider session={pageProps.session}>
+    <Header />
+    <Component {...pageProps} />
+  </NextAuthProvider>
   )
 }
 
