ba725d2145c0b81e09417e6e50f0f923b99144ee
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-03-10 23:52:21 -0300

    error OAuthCallbackError

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,37 @@
+import { query as q } from 'faunadb'
 import NextAuth from 'next-auth'
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
+
 
 export default NextAuth({
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            authorization: {
-                params: {
-                  scope: 'read:user',
-                },
-              },
-        })
-    ]
-})
+  providers: [
+      GithubProvider({
+          clientId: process.env.GITHUB_CLIENT_ID,
+          clientSecret: process.env.GITHUB_CLIENT_,
+          authorization: {
+              params: {
+                  scope: 'read:user, user:email'
+              }
+          },
+      }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        const { email } = user;
+                  
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: {email}}
+          )
+        )
+      
+        return true;
+      } catch(err) {
+        console.log(err)
+        return false
+      }
+  },
+}})
