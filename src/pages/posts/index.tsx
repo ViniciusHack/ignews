4578dd745af58019e08c69fd60349c825e3ba025
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-03-14 21:26:05 -0300

    chore: use the prismic api

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import Prismic from '@prismicio/client';
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
 import styles from './styles.module.scss';
 
 
@@ -29,4 +32,20 @@ export default function Posts() {
             </main>
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query(
+        [Prismic.predicates.at('document.type', 'post')], 
+        {
+            fetch: ['post.title', 'post.content'],
+            pageSize: 100,
+        }
+    )
+
+    return {
+        props: {}
+    }
 }
